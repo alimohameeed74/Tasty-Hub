9a78682e5c1dd645092bf93e3a51819ace6070ce
aliimohamed <aliimohamed2003@gmail.com>
2026-02-21 17:11:47 +0200

    End of project structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,46 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'auth',
+    pathMatch: 'full',
+  },
+  {
+    path: 'auth',
+    loadComponent: () => import('../app/pages/login/login.component').then((c) => c.LoginComponent),
+    title: 'Tasty Hub | Login',
+  },
+  {
+    path: 'home',
+    loadComponent: () => import('../app/pages/home/home.component').then((c) => c.HomeComponent),
+    title: 'Tasty Hub | Home',
+  },
+  {
+    path: 'about',
+    loadComponent: () => import('../app/pages/about/about.component').then((c) => c.AboutComponent),
+    title: 'Tasty Hub | About',
+  },
+  {
+    path: 'meals',
+    loadComponent: () => import('../app/pages/meals/meals.component').then((c) => c.MealsComponent),
+    title: 'Tasty Hub | Meals',
+  },
+  {
+    path: 'team',
+    loadComponent: () => import('../app/pages/team/team.component').then((c) => c.TeamComponent),
+    title: 'Tasty Hub | Team',
+  },
+  {
+    path: 'contacts',
+    loadComponent: () =>
+      import('../app/pages/contacts/contacts.component').then((c) => c.ContactsComponent),
+    title: 'Tasty Hub | Contacts',
+  },
+  {
+    path: '**',
+    loadComponent: () =>
+      import('../app/pages/not-found/not-found.component').then((c) => c.NotFoundComponent),
+    title: '404',
+  },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavBarComponent } from './fixed-components/nav-bar/nav-bar.component';
+import { FooterComponent } from './fixed-components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavBarComponent, FooterComponent],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('Task_3');
